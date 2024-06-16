53587e87b0ff1af0c1f01668da509ca5579bc079
Alexander Blümer <94956384+YoshiB33@users.noreply.github.com>
2024-06-16 14:43:11 +0200

    Added a basic layout and buttons to navigate back and to the settings page.

--- ViewModels/ServerOverviewViewModel.cs
+++ ViewModels/ServerOverviewViewModel.cs
@@ -1,5 +1,24 @@
-﻿namespace Shoebill.ViewModels;
+﻿using System.Reactive;
+using ReactiveUI;
+using Shoebill.Services;
+
+namespace Shoebill.ViewModels;
 
 public class ServerOverviewViewModel : ViewModelBase
 {
+    public ReactiveCommand<Unit, Unit> NavigateBackCommand { get; set; }
+    public ReactiveCommand<Unit, Unit> NavigateSettingsCommand { get; set; }
+    private readonly INavigationService _navigationService;
+    public ServerOverviewViewModel(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+
+        NavigateBackCommand = ReactiveCommand.Create(NavigateBack);
+        NavigateSettingsCommand = ReactiveCommand.Create(NavigateSettings);
+    }
+
+    private void NavigateBack() =>
+        _navigationService.NavigateBack();
+    private void NavigateSettings() =>
+        _navigationService.RequestNaviagtion<SettingsViewModel>();
 }
