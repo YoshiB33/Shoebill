0b9ebcb5a05172e3f373b65d9ae3742a65063788
Alexander Blümer <94956384+YoshiB33@users.noreply.github.com>
2024-07-02 13:06:18 +0200

    Add record for the GetAccount() response

---
+++ Models/Api/Responses/GetAccountDetails.cs
@@ -0,0 +1,5 @@
+﻿namespace Shoebill.Models.Api.Responses;
+
+public record GetAccountDetails(string Object, GetAccountAttributes Attributes);
+
+public record GetAccountAttributes(int Id, bool Admin, string Username, string Eamil, string First_Name, string Last_Name, string Language);
